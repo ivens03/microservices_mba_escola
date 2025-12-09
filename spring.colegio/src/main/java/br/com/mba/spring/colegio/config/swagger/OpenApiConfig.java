3881cf6709dc21533cec1203eef3bf35b6847bfc
Ivens magno <ivensmagno@gmail.com>
2025-12-08 21:55:30 -0300

    primeiro commit

---
+++ spring.colegio/src/main/java/br/com/mba/spring/colegio/config/swagger/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package br.com.mba.spring.colegio.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Back-end - Colegio Docuemntação")
+                        .description("Documentação de APIs do colegio")
+                        .version("0.0.1")
+                        .contact(new Contact()
+                                .name("Ivens Magno Da Costa Lisboa")
+                                .email("ivensmagno@gmail.com")));
+    }
+
+}
